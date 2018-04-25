7fdce57194bd98ba089583cc4c8f985e4b90832d
Jaini GUevara <jaini.guevara@gmail.com>
2018-04-25 10:21:28 +0800

    #42518 added projects page

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        title: 'Projects Page',
+    });
+})
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         title: 'About Page',
